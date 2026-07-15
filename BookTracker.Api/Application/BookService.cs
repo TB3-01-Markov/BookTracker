16177124aa49db0b34103718bbd19bc4ff7f4c14
TB3-01-Markov <markovconstantin1@gmail.com>
2026-07-15 09:00:38 +0200

    intermedium commit

--- BookTracker.Api/Application/BookService.cs
+++ BookTracker.Api/Application/BookService.cs
@@ -1,4 +1,6 @@
 ﻿using BookTracker.Api.Application.BookList;
+using BookTracker.Api.Application.CreateBook;
+using BookTracker.Api.Domain;
 using BookTracker.Api.Storage;
 
 namespace BookTracker.Api.Application;
@@ -17,5 +19,24 @@ public class BookService(IBookRepository bookRepository)
         // Gebruik LINQ om de entiteiten in `books` te mappen naar een `BookInfo` lijst. 
         // return [.. summary];
         return summary.ToList();
+    }
+
+    public async Task<CreateBookResponse> CreateBook(CreateBookRequest request)
+    {
+        var book = new Book{
+            // map de velden van request naar de properties van dit nieuwe object
+            Title = request.Title,
+            Author = request.Author,
+            Year = request.Year
+        };
+        var savedBook = await bookRepository.AddAsync(book);
+        return new CreateBookResponse
+            {
+            // map de velden van de `savedBook` entiteit naar de properties van de response DTO
+            Id = savedBook.Id,
+            Title = savedBook.Title,
+            Author = savedBook.Author,
+            Year = savedBook.Year
+        };
     }
 }
