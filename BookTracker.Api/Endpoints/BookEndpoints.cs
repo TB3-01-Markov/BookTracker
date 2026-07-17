781756f8044152ef6ee6d6656931f9119edbe6dc
TB3-01-Markov <markovconstantin1@gmail.com>
2026-07-17 11:59:22 +0200

    namidag

---
+++ BookTracker.Api/Endpoints/BookEndpoints.cs
@@ -0,0 +1,58 @@
+﻿using BookTracker.Api.Application;
+using BookTracker.Api.Application.CreateBook;
+using BookTracker.Api.Application.UpdateBook;
+using BookTracker.Api.Domain;
+
+namespace BookTracker.Api.Endpoints;
+
+public static class BookEndpoints
+{
+    public static IEndpointRouteBuilder MapBookEndpoints(this IEndpointRouteBuilder app)
+    {
+        app.MapGet("/books", GetAllBooks);
+        app.MapGet("/books/{id:int}", GetBookById);
+        app.MapPost("/books", CreateBook);
+        app.MapPut("/books/{id:int}", UpdateBook);
+        // add the missing mapping for the DELETE route here
+        app.MapDelete("/books/{id:int}", DeleteBook);
+        return app;
+    }
+
+    public static async Task<IResult> GetAllBooks(BookService service)
+    {
+        var books = await service.GetAllBooks();
+        return Results.Ok(books);
+    }
+
+    public static async Task<IResult> GetBookById(int id, BookService service)
+    {
+        // move the code for this endpoint from Program.cs to here
+        var book = await service.GetBookById(id);
+        return Results.Ok(book);
+    }
+
+    public static async Task<IResult> CreateBook(CreateBookRequest request, BookService service)
+    {
+        // move the code for this endpoint from Program.cs to here
+        return Results.Ok(service.CreateBook(request));
+
+    }
+
+    public static async Task<IResult> UpdateBook(int id, UpdateBookRequest request, BookService service)
+    {
+        var updated = await service.UpdateBook(id, request);
+
+        if (!updated)
+        {
+            return Results.NotFound();
+        }
+
+        return Results.NoContent();
+    }
+
+    public static async Task<IResult> DeleteBook(int id, BookService service)
+    {
+        // move the code for this endpoint from Program.cs to here
+        return Results.Ok(service.DeleteBook(id));
+    }
+}
